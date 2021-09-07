80313bdfe05e51b09dffba35b87994535f29ef33
Jamie Dunsmore <my0861@freia018.hpc.l>
2021-09-07 12:49:42 +0100

    zeeman_cherab is required by ci_mastu_cherab to handle zeeman splitting

--- pycis/temp/zeeman_cherab.py
+++ pycis/temp/zeeman_cherab.py
@@ -3,11 +3,13 @@ import yaml
 import numpy as np
 from scipy.constants import h, c, e, physical_constants
 import matplotlib.pyplot as plt
+from raysect.core.math.function.float import Arg1D, Constant1D
+
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-def zeeman(bfield, view=0, stokes=False):
+def zeeman_cherab():
     """
     Zeeman-split line component wavelengths, relative intensities and polarisation states.
 
@@ -26,26 +28,27 @@ def zeeman(bfield, view=0, stokes=False):
     TODO: this script is unfinished, need to account for view_angle and polarisation effects
     """
 
-    bfield_mag = np.abs(bfield)  # TODO
     mu_b = physical_constants['Bohr magneton in eV/T'][0]
-
-    # convert view angle to radians
-    view = np.pi * view / 180
-
 
     with open(os.path.join(dir_path, 'ciii.yaml')) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
     no_transitions = len(config)
-    wls = []
-    norm_rel_ints = []
-    norm_stokes_vector = []
+
+    pi_components = []
+    sigma_plus_components = []
+    sigma_minus_components = []
 
     # loop over each transition
     for ii in range(no_transitions):
-        print(ii)
+        # normalisation constant for each transition
+        if ii == 0:
+            norm_const = 2
+        elif ii == 1:
+            norm_const = 4
+        elif ii == 2:
+            norm_const = 20
 
-        rel_ints=[]
         j_u = config[ii]['j_u']
         j_l = config[ii]['j_l']
         g_u = config[ii]['g_u']
@@ -60,96 +63,67 @@ def zeeman(bfield, view=0, stokes=False):
                 delta_j = j_u - j_l
                 delta_mj = mj_u - mj_l
 
-
                 # selection rule satisfied?
                 if abs(delta_mj) > 1:
                     continue
 
                 # zeeman perturbed energy states:
-                energy_u_ii = energy_u + (mj_u * mu_b * bfield_mag * g_u)  # [eV]
-                energy_l_ii = energy_l + (mj_l * mu_b * bfield_mag * g_l)  # [eV]
-                wls.append(h * c / ((energy_u_ii - energy_l_ii) * e))  # [m]
+                energy_u_ii = energy_u + (mj_u * mu_b * Arg1D() * g_u)  # [eV]
+                energy_l_ii = energy_l + (mj_l * mu_b * Arg1D() * g_l)  # [eV]
+                wavelength = 1e9 * (h * c / ((energy_u_ii - energy_l_ii) * e))  # [nm]
+
+                # print('f', rel_int_fine_structure)
 
                 # calculate relative line intensities, expressions taken from Table 2.1, p23, S.Silburn's thesis
                 if delta_j == 1:
                     if delta_mj == 1:
                         rel_int = 0.25 * (j_u + mj_u) * (j_u - 1 + mj_u)
+                        wl_int_tuple = (wavelength, Constant1D(rel_int_fine_structure * (rel_int / norm_const)))
+                        sigma_plus_components.append(wl_int_tuple)
+
                     elif delta_mj == -1:
                         rel_int = 0.25 * (j_u - mj_u) * (j_u - 1 - mj_u)
+                        wl_int_tuple = (wavelength, Constant1D(rel_int_fine_structure * (rel_int / norm_const)))
+                        sigma_minus_components.append(wl_int_tuple)
+
                     elif delta_mj == 0:
                         rel_int = j_u ** 2 - mj_u ** 2
+                        wl_int_tuple = (wavelength, Constant1D(rel_int_fine_structure * (rel_int / norm_const)))
+                        pi_components.append(wl_int_tuple)
 
                 elif delta_j == -1:
                     if delta_mj == 1:
                         rel_int = 0.25 * (j_u + 1 - mj_u) * (j_u + 2 - mj_u)
+                        wl_int_tuple = (wavelength, Constant1D(rel_int_fine_structure * (rel_int / norm_const)))
+                        sigma_plus_components.append(wl_int_tuple)
+
                     elif delta_mj == -1:
                         rel_int = 0.25 * (j_u + mj_u + 1) * (j_u + 2 + mj_u)
+                        wl_int_tuple = (wavelength, Constant1D(rel_int_fine_structure * (rel_int / norm_const)))
+                        sigma_minus_components.append(wl_int_tuple)
+
                     elif delta_mj == 0:
                         rel_int = (j_u + 1) ** 2 - mj_u ** 2
+                        wl_int_tuple = (wavelength, Constant1D(rel_int_fine_structure * (rel_int / norm_const)))
+                        pi_components.append(wl_int_tuple)
 
                 elif delta_j == 0:
                     if delta_mj == 1:
                         rel_int = 0.25 * (j_u + mj_u) * (j_u + 1 - mj_u)
+                        wl_int_tuple = (wavelength, Constant1D(rel_int_fine_structure * (rel_int / norm_const)))
+                        sigma_plus_components.append(wl_int_tuple)
+
                     elif delta_mj == -1:
                         rel_int = 0.25 * (j_u - mj_u) * (j_u + 1 + mj_u)
+                        wl_int_tuple = (wavelength, Constant1D(rel_int_fine_structure * (rel_int / norm_const)))
+                        sigma_minus_components.append(wl_int_tuple)
+
                     elif delta_mj == 0:
                         rel_int = mj_u ** 2
+                        wl_int_tuple = (wavelength, Constant1D(rel_int_fine_structure * (rel_int / norm_const)))
+                        pi_components.append(wl_int_tuple)
 
-
-                # account for view angle. Should this be before or after normalisation?
-                if delta_mj == 0:
-                    rel_int0 = rel_int * 0.5 * (np.sin(view))**2
-                    rel_int1 = rel_int * 0.5 * (np.sin(view))**2
-                    rel_int2 = 0
-                    rel_int3 = 0
-
-                if delta_mj == 1:
-                    rel_int0 = rel_int * 0.25 * (1 + (np.cos(view))**2)
-                    rel_int1 = rel_int * -0.25 * (np.sin(view))**2
-                    rel_int2 = 0
-                    rel_int3 = rel_int * 0.5 * (np.cos(view))
-
-                if delta_mj == -1:
-                    rel_int0 = rel_int * 0.25 * (1 + (np.cos(view)) ** 2)
-                    rel_int1 = rel_int * -0.25 * (np.sin(view)) ** 2
-                    rel_int2 = 0
-                    rel_int3 = rel_int * -0.5 * (np.cos(view))
-
-                rel_ints_stokes = [rel_int0, rel_int1, rel_int2, rel_int3]
-
-                rel_ints.append(rel_ints_stokes)
-        # normalise the intensities. Does this this for each peak in the triplet separately.
-        # print(wls, rel_ints)
-        const = np.sum(item[0] for item in rel_ints)
-        for stokes_vector in rel_ints:
-            norm_stokes_vector = []
-            for component in stokes_vector:
-                norm_stokes_vector.append((component/const) * rel_int_fine_structure)
-            norm_rel_ints.append(norm_stokes_vector)
-
-    if stokes == False:
-        new_norm_rel_ints = []
-        for vector in norm_rel_ints:
-            new_norm_rel_ints.append(vector[0])
-        return wls, new_norm_rel_ints
-
-    else:
-        return wls, norm_rel_ints
-
-
-if __name__ == '__main__':
-    bfield = 0
-    wls, ris = zeeman(bfield, )
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-
-    for wl, ri in zip(wls, ris):
-        print(wl)
-        ax.plot([wl, wl, ], [0, ri],  )
-
-    plt.show()
-
+    return pi_components, sigma_plus_components, sigma_minus_components
 
 
 
