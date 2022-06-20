pycis.model.interferometer: fixing angle units error in UniaxialCrystal.get_fringe_frequency() method.

pycis/model/interferometer.py
@@ -265,14 +265,13 @@ class UniaxialCrystal(LinearRetarder):
         ne, no = get_refractive_indices(wavelength, self.material, **kwargs)
 
         # from first-order approx. of the Veiras formula.
-        factor = (no ** 2 - ne ** 2) * np.sin(radians(self.cut_angle)) * np.cos(radians(self.cut_angle)) / \
-                 (ne ** 2 * np.sin(radians(self.cut_angle)) ** 2 + no ** 2 * np.cos(radians(self.cut_angle)) ** 2)
-        freq = self.thickness / (wavelength * focal_length) * factor
-
-        freq_x = freq * np.cos(self.orientation)
-        freq_y = freq * np.sin(self.orientation)
-
-        return freq_x, freq_y
+        cut_angle_rad = radians(self.cut_angle)
+        factor = (no ** 2 - ne ** 2) * np.sin(cut_angle_rad) * np.cos(cut_angle_rad) / \
+                 (ne ** 2 * np.sin(cut_angle_rad) ** 2 + no ** 2 * np.cos(cut_angle_rad) ** 2)
+        kmag = self.thickness / (wavelength * focal_length) * factor
+        kx = kmag * np.cos(radians(self.orientation))
+        ky = kmag * np.sin(radians(self.orientation))
+        return kx, ky
 
 
 class Waveplate(UniaxialCrystal):

pycis/temp/n_delays.py
@@ -1,4 +1,5 @@
 from math import comb
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -11,12 +12,74 @@ def get_n_possible_delays(n):
             l2.append(comb(n, s) * comb(n - s, k - s))
         l1.append(np.array(l2).sum())
     diff = int(0.5 * np.array(l1).sum())
-
     print(n, ':', sum + diff)
 
 
+def get_n_possible_delays_v2(n):
+    t1 = 1 / 2 * (2 ** n - 1)
+    l1 = []
+    for k in range(1, n + 1):
+        l2 = []
+        for s in range(1, k + 1):
+            l2.append(comb(n, k) * comb(k, s))
+        l1.append(np.array(l2).sum())
+    t2 = 0.5 * np.array(l1).sum()
+    print(n, ':', t1 + t2)
+
+
+def get_n_possible_delays_v3(n):
+    l1 = []
+    for k in range(0, n + 1):
+        l2 = []
+        for s in range(0, k + 1):
+            l2.append(comb(n, s) * comb(n-s, k-s))
+        l1.append(np.array(l2).sum())
+    t = 0.5 * (np.array(l1).sum() - 1)
+    print(n, ':', t)
+
+
+def test():
+    def gen(n):
+        l1 = []
+        for k in range(1, n + 1):
+            l2 = []
+            for s in range(1, k + 1):
+                l2.append(comb(n, k) * comb(k, s))
+            l1.append(np.array(l2).sum())
+        return np.array(l1).sum()
+
+    input = np.arange(1, 12)
+    output = np.array([gen(n) for n in input], dtype=int)
+
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.plot(input, output, 'x-')
+    ax.plot(input, 2 ** (1.582 * input), 'x-')
+    plt.show()
+
+
+def plot():
+
+    def unique(n): return 0.5 * (3 ** n - 1)
+    def possible(n): return 2 ** (2 * n - 1) - 2 ** (n - 1)
+
+    x = np.arange(1, 6)
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.plot(x, unique(x), 'o-', label='unique')
+    ax.plot(x, possible(x), 'o-', label='possible')
+    leg = plt.legend(frameon=False)
+    plt.show()
+
+
 if __name__ == '__main__':
-    for i in range(1, 20):
+    # plot()
+    for i in range(1, 10):
+        # print('i', i)
         get_n_possible_delays(i)
-        # print(1 / 2 * (2 ** (2 * i) - (2 * i)))
+        get_n_possible_delays_v2(i)
+        get_n_possible_delays_v3(i)
+        print(i, ':', (0.5 * (3 ** i - 1)))
 